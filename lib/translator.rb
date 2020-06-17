7f5502d476949784fe702290e5a6cf299891732a
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-17 16:18:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,13 +4,13 @@ require 'pry'
 #convert YAML file to a file ruby can interpret
 #load_library return a hash where each key is the name of an emoticon
 def load_library(file_path)
-  file_path = YAML.load_file('/lib/emoticons.yml')
+  emoticons = YAML.load_file('/lib/emoticons.yml')
   new_hash = {}
 
-  file_path.each do |key, value|
+  emoticons.each do |key, value|
   new_hash[key] = {}
-  new_hash[key][:english] = file_path[key][0]
-  new_hash[key][:japanese] = file_path[key][1]
+  new_hash[key][:english] = emoticons[key][0]
+  new_hash[key][:japanese] = emoticons[key][1]
 
   end
   new_hash
@@ -20,9 +20,9 @@ end
 def get_japanese_emoticon(file_path, emoticon)
 # will take a traditional Western emoticon (i.g. :)) and translate it to its Japanese version ((＾ｖ＾)).
 #It will also rely load_library to first load the YAML file.
-#  emoticons = load_library(file_path) #load library
-#  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
-  #emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+  emoticons = load_library(file_path) #load library
+  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
+  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
 
 end
 
