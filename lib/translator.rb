6750d24da982b0408544629861cbd01e98d351fd
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-17 16:38:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 
-def get_english_meaning(file_path, emoticons)
+def get_english_meaning(file_path, emoticon)
   #takes a Japanese emoticon and returns its name in English.
   #This method will rely on load_library to first load the YAML file.
  emoticons = load_library(file_path)#load library
