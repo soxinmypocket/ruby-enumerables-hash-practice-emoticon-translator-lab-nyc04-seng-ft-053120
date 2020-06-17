7b6bb1106c97a769238a2e7c349cc34ae228c9c7
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-17 16:45:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -34,6 +34,7 @@ def get_english_meaning(file_path, jap_emoticons)
     end
   end
   "Sorry, that emoticon was not found"
+end
   #takes a Japanese emoticon and returns its name in English.
   #This method will rely on load_library to first load the YAML file.
  emoticons = load_library(file_path)#load library
