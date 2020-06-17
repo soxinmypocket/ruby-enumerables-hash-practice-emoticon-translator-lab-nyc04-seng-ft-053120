50f2cd8a57222816ff08465506cbb560a41b2dfb
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-17 16:13:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ require 'pry'
 def load_library(emoticons)
   emoticons = YAML.load_file('/lib/emoticons.yml')
   new_hash = {}
-  
+
   emoticons.each do |key, value|
   new_hash[key] = {}
   new_hash[key][:english] = emoticons[key[0]]
