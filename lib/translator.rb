84015beb565e096dcac56a695de70ef8c8e8145f
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-17 15:42:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,21 +4,17 @@ require 'pry'
 #convert YAML file to a file ruby can interpret
 #load_library return a hash where each key is the name of an emoticon
 def load_library(file_path)
-  file = File.read(file_path)
-  emoticons = YAML.load(file)
-
+  emoticons = YAML.load_file(file_path)
+#Each emoticon name should point to a nested hash containing two keys, :english and :japanese. 
+#These keys will point to English and Japanese versions of the emoticon.
+new_hash = {}
+emoticons.each do |key, value|
 end
-#return a hash where each key is the name of an emoticon.
 
-# hash = {}
-# YAML.load_file(file_path).each do |meaning, emoji|
-#        hash["get_meaning"][emoji[1]] = meaning
-#        hash["get_emoticon"][emoji[0]] = emoji[1]
-#     end
-#      return hash
 
 def get_japanese_emoticon(file_path, emoticon)
-
+# will take a traditional Western emoticon (i.g. :)) and translate it to its Japanese version ((＾ｖ＾)). 
+#It will also rely load_library to first load the YAML file.
   emoticons = load_library(file_path) #load library
   emoji = emoticons.values.find {|faces| faces[0] == emoticon}
   emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
@@ -26,7 +22,9 @@ def get_japanese_emoticon(file_path, emoticon)
 end
 
 def get_english_meaning(file_path, emoticon)
-  emoticons = load_library(file_path)
+  #takes a Japanese emoticon and returns its name in English. 
+  #This method will rely on load_library to first load the YAML file.
+  emoticons = load_library(file_path)#load library
   emoji = emoticons.find {|name, faces| faces[1] == emoticon}
   emoji == nil ? "Sorry, that emoticon was not found" : emoji[0]
 end
