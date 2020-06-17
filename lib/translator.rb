5435ba53fadea4576a463fd77231e99a1145aaf8
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-17 16:21:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,8 +23,7 @@ def get_japanese_emoticon(file_path, emoticon)
   emoticons = load_library(file_path) #load library
   emoji = emoticons.values.find {|faces| faces[0] == emoticon}
   emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
-
-  end
+end
 
 
 def get_english_meaning(file_path, emoticon)
